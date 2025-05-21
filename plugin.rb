d819e82c98f334fb5d1e8b069439355f2b26cf7f
Jeffrey Wu <wu19930628@gmail.com>
2025-05-21 23:39:58 +0800

    FIX: remove js

--- plugin.rb
+++ plugin.rb
@@ -7,9 +7,13 @@
 after_initialize do
   Topic.register_custom_field_type('tlp_show_thumbnail', :boolean)
 
-  PostRevisor.track_topic_field(:tlp_show_thumbnail) do |tc, new_val|
-    tc.topic.custom_fields['tlp_show_thumbnail'] = new_val
+  PostRevisor.track_topic_field(:tlp_show_thumbnail) do |tc, v|
+    tc.topic.custom_fields['tlp_show_thumbnail'] = v
   end
+
+  # ★ 讓 TopicList 一次把欄位帶回，避免 N+1
+  TopicList.preloaded_custom_fields << 'tlp_show_thumbnail' if
+    TopicList.respond_to?(:preloaded_custom_fields)
 
   DiscourseEvent.on(:topic_previews_ready) do
     next unless defined?(::TopicPreviews::TopicListItemSerializerExtension)
