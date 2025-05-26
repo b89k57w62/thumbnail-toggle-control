745def3959cd793eff730f7c0cd5384ad752fd03
Jeffrey Wu <wu19930628@gmail.com>
2025-05-26 14:30:57 +0800

    adjust

--- plugin.rb
+++ plugin.rb
@@ -67,23 +67,60 @@ after_initialize do
     # 將來可能的操作，例如通知、日誌等
   end
 
-  # 10. 整合 TLP 元件 (如果存在)
-  DiscourseEvent.on(:topic_previews_ready) do
-    if defined?(::TopicPreviews)
-      # 修補 TLP 的序列化器 (只在伺服器端)
-      if defined?(::TopicPreviews::TopicListItemSerializerExtension)
-        module ::TopicPreviews
-          module ThumbTogglePatch
-            def thumbnail_url
-              return nil unless object.tlp_show_thumbnail
-              super
-            end
-          end
+  # 10. 整合 TLP 元件 - 修補序列化器
+  # 我們需要修補 TopicListItemSerializer 的 thumbnail_url 方法
+  add_to_serializer(:topic_list_item, :thumbnail_url, false) do
+    # 如果 tlp_show_thumbnail 為 false，返回 nil
+    return nil unless object.tlp_show_thumbnail
+    
+    # 否則調用原始的 thumbnail_url 方法（如果存在）
+    if defined?(super)
+      super
+    else
+      # 如果沒有原始方法，嘗試獲取第一張圖片
+      if object.image_url.present?
+        object.image_url
+      elsif object.archetype != Archetype.private_message
+        # 嘗試從第一個帖子獲取圖片
+        post = object.first_post || object.posts.first
+        if post&.image_url.present?
+          post.image_url
         end
+      end
+    end
+  end
 
-        ::TopicPreviews::TopicListItemSerializerExtension.prepend(
-          ::TopicPreviews::ThumbTogglePatch
-        )
+  # 11. 如果 TLP sidecar 插件存在，修補其序列化器
+  if defined?(::TopicPreviews)
+    Rails.logger.info "TLP Sidecar 插件已檢測到，正在修補序列化器"
+    
+    # 修補 TopicListItemEditsMixin 的 thumbnail_url 方法
+    if defined?(::TopicListItemEditsMixin)
+      ::TopicListItemEditsMixin.class_eval do
+        alias_method :original_thumbnail_url, :thumbnail_url
+        
+        def thumbnail_url
+          # 檢查 tlp_show_thumbnail 狀態
+          return nil unless object.tlp_show_thumbnail
+          original_thumbnail_url
+        end
+      end
+    end
+    
+    # 也修補 TopicListItemSerializer 如果它被 TLP 擴展
+    TopicListItemSerializer.class_eval do
+      alias_method :original_thumbnail_url, :thumbnail_url if method_defined?(:thumbnail_url)
+      
+      def thumbnail_url
+        # 檢查 tlp_show_thumbnail 狀態
+        return nil unless object.tlp_show_thumbnail
+        
+        if respond_to?(:original_thumbnail_url)
+          original_thumbnail_url
+        else
+          # 回退到基本實現
+          object.image_url
+        end
       end
     end
   end
